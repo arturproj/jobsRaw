a1f2590465403a5f269219eaa18cfed9b9167d88
Artur DevOps <50926041+arturproj@users.noreply.github.com>
2020-08-21 18:03:27 +0200

    dev

--- routes/jobRaw.js
+++ routes/jobRaw.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+const DataQRaw = require('./../models/jobRaw.json')
 
-const Schema = mongoose.Schema;
+const jobSearch = (jobs,keyword) => {
 
-const JobsRaw = new Schema({
-    _id: mongoose.isValidObjectId,
-    title: String,
-    tags: Array,
-    contract: String,
-    duration: String,
-    description: String,
-    publishedDate: Object,
-    salary: String,
-    city: String,
-    company: String,
-    country: String,
-    source: String,
-    link: String,
-    extras: Object,
-});
+    // Création de mots-clés
+    keyword = keyword.toLowerCase().split(" ").join("|");
+    console.log(':keyword:',keyword)
+    // Initialiser la variable de liste finale
+    const res = Array();
 
-router.use('/', (req, res) => {
-    console.log(':GET:',req.url)
-    res.json([{"title":"Stage community manager h/f paris","tags":["Reseaux sociaux","web","photoshop","wordpress"],"contract":"internship","duration":null,"description":"STAGE WEB COMMUNITY MANAGEMENT\nStage en télétravail essentiellement, durée 1-2 mois\nEn lien avec une équipe jeune, sympa et bilingue Anglais-Francais\n\nPROFIL\nÉtudiant(e) en communication ou de formation digitale\nPassionné de véhicules luxueux et rares \nBonne connaissance des médias et des réseaux sociaux\nCréatif, plein d'idées, et autonome\nConnaissance de wordpress et photoshop serait un plus\n\nMISSION:\nDévelopper notre présence sur le web\nAnimer nos pages sur les différentes plateformes\nGestion des campagnes de communication commerciales\nCréation de contenus\n\nAVANTAGES\nParticipation possible aux shootings photos\nTickets restaurants\nPrimes\nTravail à distance\nHoraires flexibles\nPerspective d'évolution vers CDD ou CDI\n\nVous êtes intéressé(e) par cette opportunité de stage, merci de postuler rapidement depuis cette annonce","publishedDate":{"$$date":1591826400000},"salary":null,"city":"Paris","company":"LSPA","country":"france","source":"https://www.aidostage.com","link":"/stage/offre-stage-community-manager-92411.html","extras":{"industry":"Communication - Publicité - Journalisme","isRemoteAvailable":true},"_id":"00M4SQO1HHfTGRwa"},JobsRaw])
+    jobs.map((job,i)=>{      
+        // Lancement de mots clés  
+        reg = new RegExp(keyword,"gi")
+
+        // Résultat de la recherche
+        var value = ( job["description"].match(reg) === null ? 
+            ( job["tags"].join(" ").match(reg)  === null ? 
+                job["title"].match(reg)
+                : job["tags"].join(" ").match(reg) )            
+            : job["description"].match(reg) 
+        );
+        if( value !== null && value.length > 0  ){
+            //console.log("arr rex", value)
+            job['keywords'] = value;
+            res.push(
+                //{ [i] : value }
+                job
+            )
+        }               
+        
+    })
+    return res;
+}
+
+router.use('/:keys', (req, res) => {
+    console.log(':GET:',req.url, req.params.keys)
+    const results = jobSearch(DataQRaw, req.params.keys);
+    res.json({
+        found : results.length,
+        results
+    })
+    //res.json("OK")
     
 });
 
